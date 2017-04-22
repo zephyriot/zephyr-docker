bdafa13e614a6e27555ad1641d64a1a441ce203a
Anas Nashif <anas.nashif@intel.com>
2017-04-22 15:54:19 -0400

    build modified gitlint
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- install.sh
+++ install.sh
@@ -39,4 +39,7 @@ echo "=============== Successfully Installed zephyr deps ============"
 wget https://launchpad.net/ubuntu/+archive/primary/+files/ccache_3.3.3-1_amd64.deb
 dpkg -i ccache_3.3.3-1_amd64.deb
 
-pip install gitlint
+
+git clone https://github.com/zephyriot/gitlint.git
+cd gitlint
+python setup.py install
